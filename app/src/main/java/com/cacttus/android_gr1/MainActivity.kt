940cdf2f866e6c0a60005f8ee064946173c7db17
Muala Kabashi <keds30955@KEDS.kv>
2024-11-18 00:34:56 +0100

    first commit

---
+++ app/src/main/java/com/cacttus/android_gr1/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.cacttus.android_gr1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.snapchat_activity)
+    }
+}
