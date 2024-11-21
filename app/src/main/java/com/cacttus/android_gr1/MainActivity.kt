dd6103fdf2b20de319b696d274a4f7e00900ba17
Muala Kabashi <keds30955@KEDS.kv>
2024-11-21 12:39:55 +0100

    bind UI elements to code

--- app/src/main/java/com/cacttus/android_gr1/MainActivity.kt
+++ app/src/main/java/com/cacttus/android_gr1/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.cacttus.android_gr1
 
 import android.os.Bundle
+import android.widget.EditText
 import androidx.activity.ComponentActivity
+import androidx.appcompat.widget.AppCompatButton
 
 class MainActivity : ComponentActivity() {
+    private lateinit var editText: EditText
+    private lateinit var button: AppCompatButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.relative_layout)
+        setContentView(R.layout.calculator_layout)
+
+        bindViews()
+
+        button.setOnClickListener {
+            editText.setText(button.text)
+        }
+    }
+    fun bindViews(){
+        editText = findViewById(R.id.number)
+        button = findViewById(R.id.btn4)
     }
 }
+
